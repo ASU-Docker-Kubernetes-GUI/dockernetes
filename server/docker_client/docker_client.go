bc648b90072f61a4a659c55f859f7995567354ce
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-01-23 10:35:16 -0700

    Update docker_client.go, fleshing out routes

--- server/docker_client/docker_client.go
+++ server/docker_client/docker_client.go
@@ -3,68 +3,134 @@ package client
 import (
 	"context"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	docker "github.com/docker/docker/client"
 	"io"
+	"strings"
 )
 
 type dockerClient struct {
-	ctx *context.Context
-	dockerClient *docker.Client
+	ctx            *context.Context
+	dockerClient   *docker.Client
+	containerCache map[string]types.Container
+	imageCache     map[string]bool
 }
-
 
 type DockerClient interface {
 	GetAllContainers() (*[]types.Container, error)
-	GetContainerById(ID string) (*types.Container, error)
-	RunContainer(image string) (*types.Container, error)
+	GetContainerById(ID string) (types.Container, error)
+	CreateContainer(imageName, containerName string) (*types.Container, error)
 	GetContainerLogs(containerName string) (*io.ReadCloser, error)
 	StopAllContainers() error
 	RestartContainer(containerID string) error
 }
 
-//NewDockerClient creates a new Docker Client with a provided context
+// NewDockerClient creates a new Docker Client with a provided context
 func NewDockerClient(ctx *context.Context) *dockerClient {
 	client, err := docker.NewClientWithOpts(docker.FromEnv, docker.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	return &dockerClient{ctx: ctx, dockerClient: client}
+	c := make(map[string]types.Container)
+	i := make(map[string]bool)
+
+	return &dockerClient{ctx: ctx, dockerClient: client, containerCache: c, imageCache: i}
 }
 
-//GetAllContainers
+// GetAllContainers
 func (d *dockerClient) GetAllContainers() (*[]types.Container, error) {
-	// We can add additional logic later on
 	response, err := d.dockerClient.ContainerList(*d.ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		return nil, err
 	}
+
+	// Store the containers in the cache. This way when we call `GetContainerById` we can immediately return the result
+	for _, container := range response {
+		if con, ok := d.containerCache[container.ID]; !ok {
+			d.containerCache[container.ID] = container
+		} else {
+			d.containerCache[container.ID] = con
+		}
+	}
+
 	return &response, nil
 }
 
-//GetContainerById
+// GetContainerById
 func (d dockerClient) GetContainerById(ID string) (*types.Container, error) {
-	panic("implement me")
+	if container, ok := d.containerCache[ID]; ok {
+		return &container, nil
+	}
+
+	response, err := d.dockerClient.ContainerList(*d.ctx, types.ContainerListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, container := range response {
+		if c, ok := d.containerCache[container.ID]; !ok {
+			d.containerCache[container.ID] = container
+		} else {
+			d.containerCache[c.ID] = c
+		}
+	}
+
+	container := d.containerCache[ID]
+	return &container, nil
 }
 
-//RunContainer
-func (d dockerClient) RunContainer(image string) (*types.Container, error) {
-	panic("implement me")
+// CreateContainer
+func (d dockerClient) CreateContainer(imageName, containerName string) error {
+	// Check if the image already exists
+	if _, ok := d.imageCache[imageName]; !ok {
+		img, err := d.dockerClient.ImagePull(*d.ctx, imageName, types.ImagePullOptions{})
+		if err != nil {
+			return err
+		}
+
+		buf := new(strings.Builder)
+		_, err = io.Copy(buf, img)
+		if err != nil {
+			return err
+		}
+
+		d.imageCache[imageName] = true
+	}
+
+	cName := ""
+	if containerName != "" {
+		cName = containerName
+	}
+
+	resp, err := d.dockerClient.ContainerCreate(*d.ctx, &container.Config{
+		Image: imageName,
+	}, nil, nil, nil, cName)
+
+	if err != nil {
+		return err
+	}
+
+	// Start the container
+	if err := d.dockerClient.ContainerStart(*d.ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	// Now add the image to our image cache.
+	return nil
 }
 
-//GetContainerLogs
+// GetContainerLogs
 func (d dockerClient) GetContainerLogs(containerName string) (*io.ReadCloser, error) {
 	panic("implement me")
 }
 
-//StopAllContainers
+// StopAllContainers
 func (d dockerClient) StopAllContainers() error {
 	panic("implement me")
 }
 
-//RestartContainer
+// RestartContainer
 func (d dockerClient) RestartContainer(containerID string) error {
 	panic("implement me")
 }
-
-
