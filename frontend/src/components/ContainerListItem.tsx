1c923990076fc5a7e8ed5b54a201ff372dc1b72f
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2021-03-20 20:36:30 -0700

    Adding more stuff to container list item

--- frontend/src/components/ContainerListItem.tsx
+++ frontend/src/components/ContainerListItem.tsx
@@ -1,26 +1,8 @@
-import {
-  Button,
-  Card,
-  Elevation,
-  Colors,
-  Tag,
-  Intent,
-  Divider,
-} from '@blueprintjs/core';
-import { stat } from 'fs';
-import React, { Component, ReactElement } from 'react';
-
-export type ContainerListItemProps = {
-  name?: string; // the name of the container
-  status?: string; // The status will either be: running/stopped
-  image?: string;
-  created?: string;
-  host?: string;
-  publishedPorts?: string;
-  ownership?: string;
-};
+import { Card, Elevation, Tag, Intent, Divider } from '@blueprintjs/core';
+import React, { Component } from 'react';
 
 type ContainerStatusProps = { status: string };
+
 function ContainerStatus(props: ContainerStatusProps) {
   const { status } = props;
   const intentOptions = new Map([['running', Intent.SUCCESS]]);
@@ -34,9 +16,30 @@ function ContainerStatus(props: ContainerStatusProps) {
   );
 }
 
+interface ContainerButtonProps {
+  containerId: string;
+  handlerFunction: Function;
+}
+
+function ContainerButton(props: ContainerButtonProps) {
+  const { containerId } = props;
+}
+
+export type ContainerListItemProps = {
+  id?: string;
+  name?: string; // the name of the container
+  status?: string; // The status will either be: running/stopped
+  image?: string; // the name of the image
+  created?: string; // when the image was created
+  host?: string; // who is hosting this container
+  publishedPorts?: string; // what ports are public
+  ownership?: string; // who owns this damn container
+};
+
 class ContainerListItem extends Component<ContainerListItemProps> {
   render() {
     const {
+      id,
       name,
       status,
       image,
@@ -47,14 +50,21 @@ class ContainerListItem extends Component<ContainerListItemProps> {
     } = this.props;
 
     return (
-      <Card interactive elevation={Elevation.THREE}>
+      <Card interactive id={id} elevation={Elevation.THREE}>
         <div>
           <h2>
-            {'PrivateName  '}
-            <ContainerStatus status={'running'} />
+            {`${name}`}{' '}
+            <ContainerStatus
+              status={status === undefined ? 'running' : status}
+            />
           </h2>
           <Divider />
+          <p>Id: {id}</p>
           <p>Image Name: {image}</p>
+          <p>Created Time: {created}</p>
+          <p>Host: {host}</p>
+          <p>Published Ports: {publishedPorts}</p>
+          <p>Ownership: {ownership}</p>
         </div>
       </Card>
     );
